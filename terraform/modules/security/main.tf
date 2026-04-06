31fc23fa9247aa7d36e993e0d7ab2fd5cd5055ef
Coronado <retrollaso@gmail.com>
2026-04-06 00:01:24 -0500

    feat(main): CI/CD

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -1,4 +1,8 @@
-# --- 1. AWS WAF (Web Application Firewall) ---
+provider "aws" {
+  region = "us-west-2"
+}
+
+# --- 1. AWS WAF ---
 resource "aws_wafv2_web_acl" "main" {
   name        = "microservices-waf"
   description = "Proteccion para el ALB de pagos"
@@ -11,13 +15,16 @@ resource "aws_wafv2_web_acl" "main" {
   rule {
     name     = "AWSManagedRulesAmazonIpReputationList"
     priority = 1
+
     override_action { none {} }
+
     statement {
       managed_rule_group_statement {
         name        = "AWSManagedRulesAmazonIpReputationList"
         vendor_name = "AWS"
       }
     }
+
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "WAF_IP_Reputation"
@@ -32,7 +39,7 @@ resource "aws_wafv2_web_acl" "main" {
   }
 }
 
-# --- 2. COGNITO (User Pool para Usuarios) ---
+# --- 2. COGNITO ---
 resource "aws_cognito_user_pool" "users" {
   name = "nequi-ticketing-user-pool"
 
@@ -45,10 +52,10 @@ resource "aws_cognito_user_pool" "users" {
   }
 
   schema {
-    attribute_data_type      = "String"
-    name                     = "email"
-    required                 = true
-    mutable                  = false
+    attribute_data_type = "String"
+    name                = "email"
+    required            = true
+    mutable             = false
   }
 
   auto_verified_attributes = ["email"]
@@ -57,10 +64,14 @@ resource "aws_cognito_user_pool" "users" {
 resource "aws_cognito_user_pool_client" "client" {
   name         = "ticketing-app-client"
   user_pool_id = aws_cognito_user_pool.users.id
-  explicit_auth_flows = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
 }
 
-# --- 3. IAM ROLES (Permisos de Microservicios) ---
+# --- 3. IAM ROLES ---
 
 resource "aws_iam_role" "ecs_exec_role" {
   name = "ecs-execution-role"
@@ -93,6 +104,7 @@ resource "aws_iam_role" "ecs_task_role" {
   })
 }
 
+# Permisos microservicios
 resource "aws_iam_role_policy" "microservices_policy" {
   name = "microservices-access-policy"
   role = aws_iam_role.ecs_task_role.id
@@ -119,7 +131,45 @@ resource "aws_iam_role_policy" "microservices_policy" {
   })
 }
 
-# --- 4. SECRETS MANAGER (Password de Redis) ---
-resource "aws_secretsmanager_secret" "redis_password" {
-  name = "prod/microservices/redis-pass"
+
+resource "aws_secretsmanager_secret" "app_secrets" {
+  name        = "prod/ticketing-service"
+  description = "Secrets para microservicio ticketing"
 }
+
+resource "aws_secretsmanager_secret_version" "app_secrets_base" {
+  secret_id = aws_secretsmanager_secret.app_secrets.id
+
+  secret_string = jsonencode({
+    REDIS_HOST = "my-redis.xxxxxx.cache.amazonaws.com"
+    REDIS_PORT = "6379"
+
+    INVENTORY_REQUEST_QUEUE_URL  = "https://sqs.us-west-2.amazonaws.com/xxx/inventory-request"
+    INVENTORY_RESPONSE_QUEUE_URL = "https://sqs.us-west-2.amazonaws.com/xxx/inventory-response"
+    INVENTORY_REQUEST_DLQ_URL    = "https://sqs.us-west-2.amazonaws.com/xxx/inventory-dlq"
+
+    ORDER_TTL_QUEUE_URL     = "https://sqs.us-west-2.amazonaws.com/xxx/order-ttl"
+    INVENTORY_TTL_QUEUE_URL = "https://sqs.us-west-2.amazonaws.com/xxx/inventory-ttl"
+
+    ORDER_EXPIRATION_TOPIC_ARN = "arn:aws:sns:us-west-2:xxx:order-expiration-topic"
+
+    IDEMPOTENCY_TABLE = "Idempotency"
+  })
+}
+
+resource "aws_iam_role_policy" "secrets_access" {
+  name = "secrets-access-policy"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["secretsmanager:GetSecretValue"]
+        Resource = aws_secretsmanager_secret.app_secrets.arn
+      }
+    ]
+  })
+}
+
